9f97ea79997f6959d028c17960acb7f955640d6b
Julius Krahn <juliuskrahn@outlook.de>
2021-02-12 13:04:21 +0100

    ABC News scrape file: cache pagination

--- scrape_abc_news_archive.py
+++ scrape_abc_news_archive.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import datetime
 import re
-from utils import unprocessed_archive_urls, process_crawled_archive_response_chunk
+from utils import unprocessed_archive_urls, process_crawled_archive_response_chunk, db_connection_cursor
 import logging
+from psycopg2.extras import execute_values
 
 
 PUBLISHER = "ABCNews"
@@ -15,6 +16,11 @@ def archive_urls():
     for day in range((date_end - date_start).days + 1):
         date = date_start + datetime.timedelta(days=day)
         yield "https://www.abc.net.au/news/archive/?date=" + date.strftime("%Y-%m-%d")
+    # load cached pagination pages
+    with db_connection_cursor() as cur:
+        cur.execute("SELECT key FROM temp WHERE publisher = %s", [PUBLISHER])
+        for record in cur:
+            yield record[0]
 
 
 @process_crawled_archive_response_chunk(PUBLISHER, write_to_db=True)
@@ -41,5 +47,12 @@ def scrape_articles(resp):
 def pagination_handler(resp):
     soup = BeautifulSoup(resp.text, "lxml")
     next_el = soup.find("a", class_="next icon-")
-    if next_el:
-        return "https://www.abc.net.au/news/archive/" + next_el.attrs["href"]
+    if not next_el:
+        return
+    url = "https://www.abc.net.au/news/archive/" + next_el.attrs["href"]
+    with db_connection_cursor() as cur:  # cache
+        execute_values(cur,
+                       "INSERT INTO temp (PUBLISHER, key) "
+                       "VALUES %s ON CONFLICT DO NOTHING",
+                       [(PUBLISHER, url)])
+    return url
